dbde06751035c8f3ca9dc19c9190152923c6fe5c
Collin Bolles <collinbolles@gmail.com>
2025-03-27 12:21:01 -0400

    chore: Frontend Setup (#1)
    * Vite Create
    * MUI Install
    * Theme setup
    * Basic routing setup
    * Header/Footer skeleton
    * Placeholder landing page
    * GitHub actions for checks
    * Copy over existing CSS

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import "./App.css";
+import { Theme } from "./theme/Theme";
+import { Navigation } from "./components/Navigation.component";
+import { Footer } from "./components/Footer.component";
+import { Container } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Landing } from "./pages/Landing.page";
+
+const App: FC = () => {
+  return (
+    <Theme>
+      <Navigation />
+      <Container component="main">
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+      <Footer />
+    </Theme>
+  );
+};
+
+export default App;
---
+++ packages/frontend/src/theme/Theme.tsx
@@ -0,0 +1,34 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import React, { FC } from "react";
+
+export interface ThemeProps {
+  children: React.ReactNode;
+}
+
+export const Theme: FC<ThemeProps> = ({ children }) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#193948",
+      },
+    },
+    typography: {
+      h1: {
+        fontSize: "3rem",
+      },
+      h2: {
+        fontSize: "30px",
+      },
+      h3: {
+        fontSize: "20px",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
