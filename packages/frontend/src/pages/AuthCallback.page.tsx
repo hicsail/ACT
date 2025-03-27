51996e54877931073726c921f38985833b10f0f4
Collin Bolles <collinbolles@gmail.com>
2025-03-27 13:42:07 -0400

    feat: Frontend Auth Setup (#2)
    * Install Casdoor requirements
    * Bring in docker compose for casdoor
    * Stub out frontend requirements

---
+++ packages/frontend/src/pages/AuthCallback.page.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { AuthCallback as Casdoor } from "casdoor-react-sdk";
+import { CasdoorSDK } from "../services/casdoor.service";
+import { config } from "../config/configuration";
+
+export const AuthCallback: FC = () => {
+  return (
+    <Casdoor
+      sdk={CasdoorSDK}
+      serverUrl={config.backendURL}
+      saveTokenFromResponse={(res) => {
+        // @ts-ignore
+        // save token
+        localStorage.setItem("token", res.data.accessToken);
+      }}
+      isGetTokenSuccessful={(res) => {
+        // @ts-ignore
+        // according to the data returned by the server,
+        // determine whether the `token` is successfully obtained through `code` and `state`.
+        return res.success === true;
+      }}
+    />
+  );
+};
