3b482b7055c4e220c657d1ff7a90e94228bf6038
Collin Bolles <collinbolles@gmail.com>
2025-03-28 12:01:28 -0400

    Add in different content for loggedin vs. not logged in user

--- packages/frontend/src/pages/Landing.page.tsx
+++ packages/frontend/src/pages/Landing.page.tsx
@@ -1,30 +1,19 @@
 import { Button, Stack, Typography } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 import { config } from "../config/configuration";
+import { useNavigate } from "react-router";
+import { useUser } from "../contexts/User.context";
 
 export const Landing: FC = () => {
-  const [loginURL, setLoginURL] = useState<string| null>(null);
-
-  const getAuthURL = async () => {
-    const result = await fetch(`${config.backendURL}/casdoor/redirect`);
-    const body = await result.json();
-    setLoginURL(body.url);
-  };
-
-  useEffect(() => {
-    getAuthURL();
-  }, []);
+  const { user } = useUser();
 
   return (
     <Stack spacing={3} alignItems="center">
       <Typography variant="h1">
         Welcome to the Performance Task Site!
       </Typography>
-      <Typography variant="body1">
-        When you are ready, please login below
-      </Typography>
 
-      {loginURL && <Button variant="contained" href={loginURL}>Login</Button>}
+      {user ?  <UserContent /> : <LoginContent />}
 
       <Typography variant="body1">
         Please reach out to the SimSE Research Team, if you have any questions
@@ -40,4 +29,44 @@ export const Landing: FC = () => {
       </Typography>
     </Stack>
   );
+};
+
+// Content shown to a person that needs to login
+const LoginContent: FC = () => {
+  const [loginURL, setLoginURL] = useState<string| null>(null);
+
+  const getAuthURL = async () => {
+    const result = await fetch(`${config.backendURL}/casdoor/redirect`);
+    const body = await result.json();
+    setLoginURL(body.url);
+  };
+
+  useEffect(() => {
+    getAuthURL();
+  }, []);
+
+  return (
+    <>
+      <Typography variant="body1">
+        When you are ready, please login below
+      </Typography>
+
+      {loginURL && <Button variant="contained" href={loginURL}>Login</Button>}
+    </>
+  )
+};
+
+// Content shown to someone who is already logged in
+const UserContent: FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Typography variant="body1">
+        When you are ready, please click the link below
+      </Typography>
+
+      <Button variant="contained" onClick={() => navigate('/home')}>Home</Button>
+    </>
+  )
 };
