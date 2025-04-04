c29f90f8ec59e663feaf7b3a80b2bb1168c80d46
Collin Bolles <collinbolles@gmail.com>
2025-04-04 14:43:29 -0400

    Simplify migration

--- packages/backend/prisma/migrations/20250404181958_add_active_flag/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `active` to the `TaskSet` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `TaskSet` ADD COLUMN `active` BOOLEAN NOT NULL;
--- packages/backend/prisma/migrations/20250404184318_task_set/migration.sql
+++ packages/backend/prisma/migrations/20250404184318_task_set/migration.sql
@@ -12,6 +12,7 @@ CREATE TABLE `TaskSet` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `active` BOOLEAN NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
