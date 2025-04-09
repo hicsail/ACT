0119750bf78fafafc6074127b8716cc9a8ab0201
Collin Bolles <collinbolles@gmail.com>
2025-04-09 17:06:37 -0400

    Add in super basic instruction screen layout

--- packages/frontend/src/pages/TaskRecording.page.tsx
+++ packages/frontend/src/pages/TaskRecording.page.tsx
@@ -58,6 +58,13 @@ export const TaskRecording: FC = () => {
       {taskCompletion && task && (
         <Stack>
           <Typography variant='h1'>{task.title}</Typography>
+          <Typography variant='body1'>TODO: Task description</Typography>
+          <Typography variant='h3'>The problem you have chosen is:</Typography>
+          <Typography variant='body1'>TODO: Add problem description</Typography>
+          <Typography variant='h3'>Your task is to do the following:</Typography>
+          <Typography variant='body1'>TODO: Provide task details</Typography>
+          <Typography variant='h3'>Remember to:</Typography>
+          <Typography variant='body1'>TODO: Task breakdown</Typography>
         </Stack>
       )}
     </>
