0377a977e164689d1aa519feafecd413eab67553
Collin Bolles <collinbolles@gmail.com>
2025-04-09 12:03:22 -0400

    Rendering of task

--- packages/frontend/src/components/TaskList.component.tsx
+++ packages/frontend/src/components/TaskList.component.tsx
@@ -1,6 +1,8 @@
 import { Stack } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { TaskEntity, tasksControllerGetActiveTasks } from '../client';
+import { ActivityCard } from './ActivityCard.component';
+import taskPreviewImage from '../assets/TaskPreviewImage.png';
 
 export const TaskList: FC = () => {
   const [taskList, setTaskList] = useState<TaskEntity[]>([]);
@@ -22,7 +24,16 @@ export const TaskList: FC = () => {
 
   return (
     <Stack>
-
+      {taskList.map((task) => (
+        <ActivityCard
+          previewImage={taskPreviewImage}
+          activityTitle={task.title}
+          activityDescription={task.preview}
+          activityEstimatedTime={task.timeSeconds + ''}
+          activityComplete={false}
+          onSelectionAction={() => console.log('hi :)')}
+        />
+      ))}
     </Stack>
   );
 }
