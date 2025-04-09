7e10b7ca6dcbf47e0d7e3fadc8b85bcc203adf1d
Collin Bolles <collinbolles@gmail.com>
2025-04-09 11:45:53 -0400

    Bring in codegen

---
+++ packages/frontend/src/components/TaskList.component.tsx
@@ -0,0 +1,10 @@
+import { Stack } from '@mui/material';
+import { FC } from 'react';
+
+export const TaskList: FC = () => {
+  return (
+    <Stack>
+
+    </Stack>
+  );
+}
---
+++ packages/frontend/src/contexts/Client.context.tsx
@@ -0,0 +1,23 @@
+import { createContext, FC, ReactNode } from 'react';
+import { client } from '../client/client.gen';
+import { config } from '../config/configuration';
+import { TOKEN_KEY } from './User.context';
+
+const ClientContext = createContext({} as typeof client);
+
+export interface ClientProviderProps {
+  children: ReactNode;
+}
+
+export const ClientProvider: FC<ClientProviderProps> = ({ children }) => {
+  client.setConfig({
+    baseUrl: config.backendURL,
+    auth: () => localStorage.getItem(TOKEN_KEY) || ''
+  });
+
+  return (
+    <ClientContext.Provider value={client}>
+      {children}
+    </ClientContext.Provider>
+  )
+};
