a25dc9e4cfeb302157e07b42b6bbc0c235f08525
Collin Bolles <collinbolles@gmail.com>
2025-04-23 15:55:21 -0400

    Working ability to set active task

---
+++ packages/admin/src/components/sets/SetActive.component.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@mui/material';
+import { FC } from 'react';
+import { useRecordContext, useRefresh } from 'react-admin';
+import { taskSetControllerSetActive } from '../../client';
+
+export const SetActive: FC = () => {
+  const refresh = useRefresh();
+
+  const record = useRecordContext();
+  if (!record) {
+    return null;
+  }
+
+  const handleSetActive = async () => {
+    const result = await taskSetControllerSetActive({
+      path: {
+        id: record['id'] as string
+      }
+    });
+
+    if (result.error) {
+      // TODO: Handle error
+      console.error(result.error);
+    }
+
+    refresh();
+  };
+
+  return (
+    <Button variant='contained' disabled={record['active']}  onClick={handleSetActive}>Set Active</Button>
+  );
+};
---
+++ packages/admin/src/components/sets/SetsList.component.tsx
@@ -0,0 +1,15 @@
+import { Datagrid, List, TextField } from 'react-admin';
+import { FC } from 'react';
+import { SetActive } from './SetActive.component';
+
+export const SetsList: FC = () => {
+  return (
+    <List>
+      <Datagrid>
+        <TextField source='name' />
+        <TextField source='description' />
+        <SetActive />
+      </Datagrid>
+    </List>
+  );
+};
