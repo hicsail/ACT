1b351cde2ad94140d0ead8323218441700972763
Collin Bolles <collinbolles@gmail.com>
2025-04-23 12:54:45 -0400

    Getting started with search abilities

--- packages/admin/src/components/taskcompletions/TaskCompletionsList.component.tsx
+++ packages/admin/src/components/taskcompletions/TaskCompletionsList.component.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
-import { BooleanField, Datagrid, List, ReferenceField, TextField } from 'react-admin';
+import { BooleanField, Datagrid, List, ReferenceField, TextInput, TextField } from 'react-admin';
 import { VideoField } from '../fields/VideoField.component';
 import { Redo } from './Redo.component';
 
+const taskCompletionsFilters = [
+  <TextInput label='User' source='userId' />
+];
+
 export const TaskCompletionsList: FC = () => {
   return (
-    <List>
+    <List filters={taskCompletionsFilters}>
       <Datagrid>
         <ReferenceField source="taskId" reference="tasks">
           <TextField source="title" />
