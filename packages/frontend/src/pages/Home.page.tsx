175f25308c7cc2da48768d6f4f6e51a21ec5d5ca
Collin Bolles <collinbolles@gmail.com>
2025-05-28 11:04:33 -0400

    Fix formatting

--- packages/frontend/src/pages/Home.page.tsx
+++ packages/frontend/src/pages/Home.page.tsx
@@ -11,14 +11,12 @@ export const Home: FC = () => {
   return (
     <>
       <Stack spacing={3} sx={{ textAlign: 'center', padding: 3 }}>
-        <Typography variant="body1">
-          Thank you for supporting our research by completing these tasks!
-        </Typography>
+        <Typography variant="body1">Thank you for supporting our research by completing these tasks!</Typography>
 
         <Typography variant="body1">
           Failure to complete assigned tasks could result in delayed compensation and additional work required. If you
-          have any questions, reach out to teachsimlab@gmail.com Don't forget to use this tip sheet to support your tasks.
-          Review this before you enter the tasks as the timer will begin immediately.
+          have any questions, reach out to teachsimlab@gmail.com Don't forget to use this tip sheet to support your
+          tasks. Review this before you enter the tasks as the timer will begin immediately.
         </Typography>
 
         <Typography variant="h2">Demonstrate your skills through the assigned tasks below.</Typography>
