e6c34c234512abeb71e96ef98db8f6c6e2381281
Collin Bolles <collinbolles@gmail.com>
2025-06-11 10:09:49 -0400

    Start with event emitters

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -11,6 +11,7 @@ import { S3Module } from './s3/s3.module';
 import { UsersModule } from './users/users.module';
 import { StudymappingModule } from './studymapping/studymapping.module';
 import configuration from './config/configuration';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true
     }),
+    EventEmitterModule.forRoot(),
     CasdoorModule,
     PrismaModule,
     TasksModule,
